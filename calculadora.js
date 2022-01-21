285dae48bd153c10514309223221dec3053168ef
Carlos <carlos.laya96@gmail.com>
2022-01-21 13:57:52 -0300

    Funcao calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,17 @@
-console.log("teste");
+console.log("calculadora");
+
+
+
+function calculadora (valor1, valor2){
+
+    let soma = valor1 + valor2;
+    let multiplacacao = valor1 * valor2;
+    let divisao = valor1 / valor2;
+    let sub = valor1 - valor2;
+
+
+    console.log(soma, multiplacacao, divisao, sub);    
+}
+
+
+calculadora(2, 3);
